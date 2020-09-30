808a1f3143791760a318b02cc83dc3f89c4997ca
csmahmoued <55460618+csmahmoued@users.noreply.github.com>
2020-09-30 18:30:42 +0200

    add documention to explain what i did

--- elnady-frontline-endpoint-service/src/main/java/com/elnady/frontline/service/PersonService.java
+++ elnady-frontline-endpoint-service/src/main/java/com/elnady/frontline/service/PersonService.java
@@ -3,6 +3,8 @@ package com.elnady.frontline.service;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,15 +15,23 @@ import com.elnady.frontline.model.Person;
 @FeignClient("elnady-persons-endpoint-service")
 public interface PersonService {
 
+	/** call /persons from persons service */
 	 @RequestMapping(method = RequestMethod.GET,value = "/persons",consumes = "application/json")
 	 public List<Person> getAllPersons();
-	 
-	 @RequestMapping(method = RequestMethod.DELETE,value = "/persons/{id}",consumes = "application/json")
-	 public void deletePerson(int id);
 
-	 @RequestMapping(method = RequestMethod.GET,value = "/persons/{id}",consumes = "application/json")
-	 public Person getPerson(int id);
+		/** call /persons/id from persons service */
+	 @RequestMapping("/persons/{id}")
+	 public void deletePerson(@PathVariable int id);
+
+		/** call /persons/{id}  from persons service to get person by id */
+	 @RequestMapping("/persons/{id}")
+	 public Person getPerson(@PathVariable int id);
 	 
+		/** call /persons{id} from persons service to update service */
+	 @RequestMapping("/persons/{id}")
+	 public Person updatePerson(@RequestBody Person person,@PathVariable int id);
+	 
+		/** call /persons from persons service to add new service */
 	 @RequestMapping(method = RequestMethod.POST,value = "/persons",consumes = "application/json")
 	 public Person addNewPerson(Person person);
 	 
